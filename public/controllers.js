0bb07ec4fd287c406344877955c7eef7f7ec23cb
Victor Tang <victortang1992@gmail.com>
2016-04-10 23:24:30 -0700

    fancy widening txt input on click

--- public/controllers.js
+++ public/controllers.js
@@ -15,5 +15,68 @@ angular.module('AlbumApp').controller('autoLogger',['$scope', 'ActiveUser',funct
 /*Page Specific Controls - Galleries   */
 /*-------------*/
 angular.module('AlbumApp').controller('MainCtrl', ['$scope', function($scope){
+	/* STYLE */
+	/* ng-style = widen form when new Gall mode */
+	$scope.widenForm = function(){
+		if(!$scope.newGallMode){
+			return {};
+		}
+		else{
+			return { "width":"200px" };
+		}
+	}
+	/* ng-style = widen form when new Gall mode */
+	$scope.showInput = function(){
+		if(!$scope.newGallMode){
+			return {};
+		}
+		else{
+			return { "opacity": 1, "visibility": "visible" };
+		}
+	}
+	/* ng-style = rotate plus when new Gall mode */
+	$scope.rotatePlus = function(){
+		if(!$scope.newGallMode){
+			return {};
+		}
+		else{
+			return { "left":"1rem", "transform": "rotate(45deg)" };
+		}
+	}
+	
+	/* STATE */ 
+	$scope.newGallMode = false;
+	/* FUNCTIONS */
+	/* toggle new gallery form */
+	$scope.newGallForm = function(){
+		if(!$scope.newGallMode){
+			$scope.newGallMode = true;
+		}
+		else{
+			$scope.newGallMode = false;
+		}
+	}
+	/* submit new gallery form */
+	$scope.newGall = function(){
+		
+	}
+	/* ng model for forms */
+	$scope.gallEdits = {
+		newGall:""
+	}
+	
+	
+	/* toggle new img link form */
+	$scope.newImgLink = function(){
+		
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
     $scope.currentGallery = ["alpha","beta","delta","phi","gamma","theta"];
 }]);//end of controller
