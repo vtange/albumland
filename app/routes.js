f6a506f23b1133848207be30ed0fe59eabf34cdc
Victor Tang <victortang1992@gmail.com>
2016-04-11 12:58:13 -0700

    checking others' pages

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,7 @@ module.exports = function(app) {
     // =====================================
     app.get('/', function(req, res) {
 		if(req.user){
+			//send user's home page
 			User.findById(req.user.id).populate('galleries').exec(function(err,user){
 				res.render('index.ejs', {
 					title: app.title,
@@ -18,12 +19,42 @@ module.exports = function(app) {
 			});
 		}
 		else{
+			//render index page with "recent stuff" galleries
+			var galleries;
 			res.render('frontpage.ejs', {
 				title: app.title,
 				user : req.user, // get the user out of session and pass to template
 				package : JSON.stringify([req.user]) //send info to angular
 			}); // load the index.ejs file
 		}
+    });
+    // =====================================
+    // SOMEONE'S PAGE ========
+    // =====================================
+    app.get('/see/:USERNAME', function(req, res) {
+		User.findOne({'local.username': req.params.USERNAME },function(err,user){
+			console.log(user);
+			if(!user){
+				//render 404 page
+				res.send(404);
+			}
+			else{
+				if(!req.user){
+					res.send("checked other user");
+				}
+				else{
+					console.log(req.user.local.username);
+					if(req.params.USERNAME === req.user.local.username){
+						console.log(req.params.USERNAME);
+						//do the same as '/'
+						res.redirect("/");
+					}
+					else{
+						res.send("checked other user");
+					}
+				}
+			}
+		});
     });
     // =====================================
     // NEW GALLERY ========
