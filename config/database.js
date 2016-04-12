37ed6e50a703c59582bf25bed348827f3e53b354
Victor Tang <victortang1992@gmail.com>
2016-04-12 15:46:49 -0700

    MongoDB URI

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' :  process.env.MONGOLAB_URI || 'mongodb://localhost/album' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' :  process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost/album' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
